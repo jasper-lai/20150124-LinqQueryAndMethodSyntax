0e2c4c80e14020bbe069b8362148f51964071121
jasper-lai <jasper.lai@gmail.com>
2015-01-23 21:12:07 +0800

    傑士伯的IT學習之路 C# LINQ:  QuerySyntax and Method Syntax with LINQPad
    傑士伯的IT學習之路 C# LINQ:  QuerySyntax and Method Syntax with LINQPad
    
    http://jasper-it.blogspot.com/2015/01/c-linq-querysyntax-and-method-syntax.html

---
+++ LinqQueryAndMethodSyntax01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ========================================
+// https://msdn.microsoft.com/en-us/library/bb397947.aspx
+// ========================================
+
+namespace LinqQueryAndMethodSyntax01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 10, 8, 3, 6, 12 };
+
+            //Query syntax:
+            IEnumerable<int> numQuery1 =
+                from num in numbers
+                where num % 2 == 0
+                orderby num
+                select num;
+
+            //Method syntax:
+            IEnumerable<int> numQuery2 = numbers.Where(num => num % 2 == 0).OrderBy(n => n);
+
+            foreach (int i in numQuery1)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine(System.Environment.NewLine);
+            foreach (int i in numQuery2)
+            {
+                Console.Write(i + " ");
+            }
+
+            // Keep the console open in debug mode.
+            Console.WriteLine(System.Environment.NewLine);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            /*
+            Output:
+            6 8 10 12
+            6 8 10 12
+           */
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,4 @@
+﻿
+傑士伯的IT學習之路 C# LINQ:  QuerySyntax and Method Syntax with LINQPad
+
+http://jasper-it.blogspot.com/2015/01/c-linq-querysyntax-and-method-syntax.html
